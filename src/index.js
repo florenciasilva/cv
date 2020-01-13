8088771627281c58119d2941961153c70124494f
florenciasilva <hensgrej@gmail.com>
2020-01-13 13:58:16 -0600

    first iteration, incomplete sections

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const underlineColors = ['paleturquoise', '#c4ffc4', 'thistle', 'bisque', 'silver', 'lightpink'];
+
+document.querySelectorAll('.title').forEach( (element, i) => {
+    element.style['text-decoration-color'] = underlineColors[i]
+});
+
+const populateParagraphs = (language) => {
+        document.querySelectorAll('.content').forEach((element, i) => {
+            console.log(language)
+            element.innerHTML = language[i];
+        });
+};
+
+const populateTitles= (language) => {
+    document.querySelectorAll('.title').forEach((element, i) => {
+        element.innerHTML = language[i];
+    });
+};
+
+document.querySelector('.esp').addEventListener('click', (event) => {
+    populateParagraphs(esp);
+    populateTitles(espTitle);
+});
+
+document.querySelector('.eng').addEventListener('click', (event) => {
+    populateParagraphs(eng);
+    populateTitles(engTitle);
+});
+
+populateParagraphs(eng);
+populateTitles(engTitle);
---
+++ src/language.js
@@ -0,0 +1,22 @@
+const eng = [ 'I am a self-taught Full Stack Developer. I\'m interested in accessibility (a11y), creating beautiful, awesome and inclusive webpages and applications. The stack that I\'m currently using every day is ReactJS, MongoDB, styled-components, and aXe. I enjoy learning new ways to use all these technologies while finding new tools on the way.',
+            'Email: hensgrej@gmail.com Portfolio: florenciasilva.github.io Github: /florenciasilva Linkedin: /florenciasilvaolivera',
+            'JavaScript | ReactJS | styled-components | HTML5 | CSS3 | a11y | Jest | Redux',
+            'NodeJS | Firebase | MongoDB | Express | JWT | bCrypt',
+            'Git | Github | Heroku | aXe | Self-learning | Creative problem solving | Agile | SCRUM | Kanban | Teamwork | Public Speaking | Accountability ',
+            'Front end coach Laboratoria | Feb 2019 - Dec 2019',
+            'Laboratoria Front End Developer | Graduated May 2018',
+            'Native Spanish | English 74/100 C2 Proficient '
+];
+
+const esp = [ 'Soy una desarrolladora full stack autodidacta. Estoy particularmente interesada en Accesibilidad (a11y), creando páginas web y aplicaciones bonitas, geniales e inclusivas. Actualmente, el stack que uso día a día es ReactJS, MongoDB, styled-components y aXe. Disfruto mucho de aprender nuevas maneras de utilizar estas tecnologías y encontrar nuevas herramientas en el camino.',
+            'Email: hensgrej@gmail.com Portfolio: florenciasilva.github.io Github: /florenciasilva Linkedin: /florenciasilvaolivera',
+            'JavaScript | ReactJS | styled-components | HTML5 | CSS3 | a11y | Jest | Redux',
+            'NodeJS | Firebase | MongoDB | Express | JWT | bCrypt',
+            'Git | Github | Heroku | aXe | Autodidacta | Solucion creativa de problemas | Agile | SCRUM | Kanban | Trabajo en equipo | Presentaciones a publico | Responsabilidad ',
+            'Front end coach Laboratoria | Feb 2019 - Dec 2019',
+            'Laboratoria Front End Developer | Graduated May 2018',
+            'Español nativo | Ingles 74/100 C2 Proficient '
+];
+
+const engTitle = [ 'Bio', 'Contact', 'Front', 'Back', 'Other', 'Work Exp', 'Education', 'Languages'];
+const espTitle = [ 'Bio', 'Contacto', 'Front', 'Back', 'Otros', 'Exp Profesional', 'Educación', 'Idiomas'];
